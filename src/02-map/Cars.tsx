4dff36f53668b1a70295add0958191b39b09b2cc
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-09-18 18:07:57 +0300

    complete map task

---
+++ src/02-map/Cars.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './Cars.module.css';
+
+type CarsType = {
+    manufacturer: string;
+    model: string;
+}
+
+type CarsListType = {
+    cars: Array<CarsType>;
+}
+
+export const Cars = (props: CarsListType) => {
+    return (
+        <>
+            <table className={styles.table}>
+                {props.cars.map((el, ind) => {
+                    return (
+                        <tr key={ind}>
+                            <td className={styles.table__cell}>{ind + 1}</td>
+                            <td className={styles.table__cell}>{el.manufacturer}</td>
+                            <td className={styles.table__cell}>{el.model}</td>
+                        </tr>
+                    )
+                })}
+            </table>
+        </>
+    )
+}
